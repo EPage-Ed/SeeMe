58e6729169a72981ae1fd8c60a9688319faee93f
Ethan Saadia <ethan@ethansaadia.com>
2021-06-12 23:42:50 -0500

    Make hostingController root VC

--- SeeMe/AppDelegate.swift
+++ SeeMe/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     application.isIdleTimerDisabled = true
+    
+    let hostingController = UIHostingController(rootView: NavigationScreen())
+    window?.rootViewController = hostingController
+      
     return true
   }
 
